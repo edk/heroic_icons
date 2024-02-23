1229048767c0eb9f73ab037811f65babd0fd801c
Ed Kim <54091+edk@users.noreply.github.com>
2024-02-23 12:06:25 -0800

    add simple test

--- test/test_heroic_icons.rb
+++ test/test_heroic_icons.rb
@@ -6,4 +6,11 @@ class TestHeroicIcons < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HeroicIcons::VERSION
   end
+
+  def test_icon_not_found
+    assert_raises(HeroicIcons::IconNotFound) do
+      HeroicIcons::Icons.new.icon('nonexistent')
+    end
+  end
+
 end
